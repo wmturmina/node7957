fa954065ded97e292001a49bf57a4a553c916480
Wendell Turmina <wmturmina@gmail.com>
2018-09-29 11:22:08 -0300

    #2

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const http = require('http')
+const port = 8443
 
 const lidaComRequests = (request, response) => {
     console.log('Servidor')
     response.end('alo alo w brazil')
 }
-
+const retornoSubirOServidor = () => {
+    console.log(`
+        Servidor subiu na porta ${port}
+        Pressione CTRL+C para derrubar
+        http://localhost:${port}
+    `)
+}
 const server = http.createServer(lidaComRequests)
-
-server.listen(8443)
+server.listen(port, retornoSubirOServidor)
