928febac953bc50d89440ef8c4633b5b67c36934
Wendell Turmina <wmturmina@gmail.com>
2018-09-29 14:45:14 -0300

    #8

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const _ = require('lodash')
+
 const port = 8443
+const livros = [
+  'React',
+  'Node',
+  'Native',
+  'Scrum'
+]
 
 const app = express()
 
@@ -7,7 +15,12 @@ const home = (request, response) => {
   response.end('<h1>HOME NEW</h1>')
 }
 const produtos = (request, response) => {
-  response.end('<h1>Lista de produtos NEW</h1>')
+  response.end(`
+  <h1>Lista de produtos NEW</h1>
+  <ol>
+    ${_.join(_.map(livros, item => `<li>${item}</li>`), '')}
+  </ol>
+  `)
 }
 
 app.get('/', home)
