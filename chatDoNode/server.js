859d51b816602c3c2bc609fd033626cc0a0ddf84
Wendell Turmina <wmturmina@gmail.com>
2018-10-13 16:12:06 -0300

    #18 Chat

---
+++ chatDoNode/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const port = 3000
+
+app.get('/', (request, response) => {
+    response.render('home.ejs')
+})
+
+http.listen(port, () => {
+    console.log(`
+        APP Iniciado na porta: ${port}
+    `)
+})
+
+io.on('connection', (socket) => {
+    console.log('Usuário conectou')
+    socket.on('CLIENT_SENDMESSAGE', (mensagem) => {
+        io.emit('SERVER_SENDMESSAGE', mensagem)
+    })
+    socket.on('disconnect', () => {
+        console.log('Usuário desconectou')
+    })
+})
